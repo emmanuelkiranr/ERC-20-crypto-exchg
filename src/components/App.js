b8e9d22af90333eb92dc9a9436972bf63db75438
Emmanuel1237 <emmanuelkiranr@gmail.com>
2020-04-06 10:25:21 -0700

    initial commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,57 @@
 import React, { Component } from 'react';
-import logo from '../logo.png';
-import './App.css';
 
-class App extends Component {
-  render() {
+import './App.css';
+import Web3 from 'web3'
+import Navbar from './Navbar.js' 
+
+class App extends Component {  //class inheritance
+
+  async componentWillMount() {         //method to load imp fns before the layout
+    await this.loadWeb3()              // to call the loadWeb3 function before all layout
+    await this.loadBlockchainData()     //fn to import blockchain data
+   // console.log(window.web3)
+  }
+
+  async loadBlockchainData() {
+    const web3 =window.web3
+
+
+    const accounts = await web3.eth.getAccounts() //get account data connected to bc
+    this.setState({account : accounts[0]})        //we set stste here so we can access this from other parts in the application 
+    //ie console log could be called from other fn .eg inside render fn
+    //console.log(accounts[0])
+    console.log(this.state.account)
+
+    const ethBalance = await web3.eth.getBalance(this.state.account) // get balance of above account
+    this.setState({ethBalance: ethBalance})
+    this.setState({ethBalance}) //since key and var are same name
+    console.log(this.state.ethBalance)
+  }
+
+ async loadWeb3() {
+    if (window.ethereum) {
+      window.web3 = new Web3(window.ethereum)
+      await window.ethereum.enable()
+    }
+    else if (window.web3) {
+      window.web3 =new Web3(window.web3.currentProvider)
+    }
+    else {
+      window.alert('Non-Ethereum browser detected. You should consider trying MetaMask!')
+    }
+  }
+
+   constructor(props) {  // store bc data in react state
+    super(props);
+    this.state = {
+
+    }
+  }
+
+  render() {           // the imported fns etc only take action once implemented in render fn.
     return (
       <div>
-        <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
-          <a
-            className="navbar-brand col-sm-3 col-md-2 mr-0"
-            href="http://www.dappuniversity.com/bootcamp"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Dapp University
-          </a>
-        </nav>
+        <Navbar account = {this.state.account} />  //react component
         <div className="container-fluid mt-5">
           <div className="row">
             <main role="main" className="col-lg-12 d-flex text-center">
@@ -25,20 +61,10 @@ class App extends Component {
                   target="_blank"
                   rel="noopener noreferrer"
                 >
-                  <img src={logo} className="App-logo" alt="logo" />
+                  
                 </a>
-                <h1>Dapp University Starter Kit</h1>
-                <p>
-                  Edit <code>src/components/App.js</code> and save to reload.
-                </p>
-                <a
-                  className="App-link"
-                  href="http://www.dappuniversity.com/bootcamp"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  LEARN BLOCKCHAIN <u><b>NOW! </b></u>
-                </a>
+                <h1>Hello world!</h1>
+              
               </div>
             </main>
           </div>
---
+++ src/components/Navbar.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+
+class Navbar extends Component {     // just to reduce the code  in app.js into one component <Navbar />
+
+  render() {           // the imported fns etc only take action once implemented in render fn.
+    return (
+      <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
+          <a
+            className="navbar-brand col-sm-3 col-md-2 mr-0"
+            href="http://www.dappuniversity.com/bootcamp"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            EthSwap
+          </a>
+          ///<p>{this.props.account}</p> for a cleaner look of account text
+          <ul className="navbar-nav px-3">
+           <li className="nav-item text-nowrap dnone d-sm-none d-sm-block">
+            <small className="text-secondary">
+              <small id="account">{this.props.account}</small>
+           </small>
+           </li>
+           </ul>
+        </nav> 
+    );
+  }
+}
+
+export default Navbar;
